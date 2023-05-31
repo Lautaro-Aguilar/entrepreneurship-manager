ef5992d1b45b3f78484de5770de5f422ca464c72
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-05-31 20:45:33 -0300

    Create the table and buttons for products

--- src/components/Productos/page.tsx
+++ src/components/Productos/page.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { useState } from 'react'
+import { Box, Button, Container, Typography } from "@mui/material";
+import { AgGridReact } from 'ag-grid-react';
+import ModalAgregar from './ModalAgregar';
+import ModalModificar from './ModalModificar';
+import ModalEliminar from './ModalEliminar';
 
 function Productos() {
+  const [openModalAgregar, setOpenModalAgregar] = useState(false)
+  const [openModalModificar, setOpenModalModificar] = useState(false)
+  const [openModalEliminar, setOpenModalEliminar] = useState(false)
+  const [rowData] = useState([
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxster", price: 72000 }
+  ]);
+
+  const [columnDefs] = useState([
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' }
+  ])
   return (
-    <div>Productos</div>
-  )
+    <Container sx={{ py: 2, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <Typography variant="h3" component="h1">
+        Registro de Productos
+      </Typography>
+
+      <Box sx={{ width: 600, my: 2 }}>
+        <Box className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+          <AgGridReact
+            rowData={rowData}
+            columnDefs={columnDefs}>
+          </AgGridReact>
+        </Box>
+
+        <Box sx={{ display: 'flex', my: 2, justifyContent: 'space-around' }}>
+          <Button variant='contained' size="large" onClick={() => setOpenModalAgregar(true)}>
+            Agregar
+          </Button>
+          <Button variant='contained' color="secondary" size="large" onClick={() => setOpenModalModificar(true)}>
+            Modificar
+          </Button>
+          <Button variant='contained' color="error" size="large" onClick={() => setOpenModalEliminar(true)}>
+            Eliminar
+          </Button>
+        </Box>
+      </Box>
+      <ModalAgregar open={openModalAgregar} setOpen={setOpenModalAgregar}/>
+      <ModalModificar open={openModalModificar} setOpen={setOpenModalModificar}/>
+      <ModalEliminar open={openModalEliminar} setOpen={setOpenModalEliminar}/>
+    </Container>
+  );
 }
 
-export default Productos
+export default Productos;
