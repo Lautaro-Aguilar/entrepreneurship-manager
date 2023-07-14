be4313fe7b6b0ee17a523291cbaddd0a3653eb56
Agustin Galante <73099311+AgustinGalante19@users.noreply.github.com>
2023-07-14 20:52:15 -0300

    resolving conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { ThemeProvider } from "@emotion/react";
-import { RouterProvider } from "react-router-dom";
-import Router from "./routes/Router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import lightTheme from "./styles/lightTheme";
 import { createContext, useEffect, useMemo, useState } from "react";
 import darkTheme from "./styles/darkTheme";
+import { AuthProvider } from "./components/Inicio/AuthProvider";
+import Root from "./routes/Root";
+import Inicio from "./components/Inicio/page";
+import Clientes from "./components/Clientes/page";
+import Orders from "./components/Orders/page";
+import Productos from "./components/Productos/page";
+import Components from "./components/Components";
+import { Dashboard } from "@mui/icons-material";
 
 interface ThemeProps {
   toggleColorMode: () => void;
@@ -51,9 +58,22 @@ function App() {
 
   return (
     <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <RouterProvider router={Router} />
-      </ThemeProvider>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <AuthProvider>
+            <Routes>
+              <Route path='/' element={<Root />}>
+                <Route index element={<Inicio />} />
+                <Route path='/Clientes' element={<Clientes />} />
+                <Route path='/Pedidos' element={<Orders />} />
+                <Route path='/Productos' element={<Productos />} />
+                <Route path='/Components' element={<Components />} />
+                <Route path='/Dashboard' element={<Dashboard />} />
+              </Route>
+            </Routes>
+          </AuthProvider>
+        </ThemeProvider>
+      </Router>
     </ColorModeContext.Provider>
   );
 }
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -4,6 +4,7 @@ import { CssBaseline } from "@mui/material";
 
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
+
 const Root = () => {
   return (
     <>
